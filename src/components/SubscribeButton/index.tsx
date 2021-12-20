ee2c5f0fb57882d261a9da0910e5b8e9642cdc32
jeffersoncharlles <contato@jefferdeveloper.com>
2021-12-20 09:22:51 -0300

    chore: subscribe create commit

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,15 +1,32 @@
+import { signIn, useSession } from 'next-auth/react';
+import { useState } from 'react';
 import styles from './styles.module.scss';
 
 interface SubsCribeProps {
     priceId: string;
 }
 
-export const SubscribeButton = ({ priceId }: SubsCribeProps) => {
+//usar credencial secret
+//getServiceProps (ssr)
+//getStaticProps(SSG)
+//API routes
 
+export const SubscribeButton = ({ priceId }: SubsCribeProps) => {
+    const { data: session } = useSession();
+
+    const handleSubscribe = () => {
+        if (!session) {
+            signIn('github');
+            return;
+        }
+
+        //created da checkout session
+    }
     return (
         <button
             type='button'
             className={styles.subscribeButton}
+            onClick={handleSubscribe}
         >
             Subscribe Now
         </button>
