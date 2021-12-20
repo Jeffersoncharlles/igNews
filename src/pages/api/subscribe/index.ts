50394370095a9e798ec5f99976d087ed2209f03f
jeffersoncharlles <contato@jefferdeveloper.com>
2021-12-20 10:56:00 -0300

    chore: not duplicate customer pay commit

--- src/pages/api/subscribe/index.ts
+++ src/pages/api/subscribe/index.ts
@@ -2,21 +2,55 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
 import { fauna } from "../../../services/fauna";
 import { stripe } from "../../../services/stripe";
-import { query as q } from 'faunadb'
+import { query as q } from 'faunadb';
+
+
+type User = {
+    ref: {
+        id: string;
+    }
+    data: {
+        stripe_customer_id: string;
+    }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+
+    //criar novo pedido de pagamento e evitar criacao duplicada
+
     if (req.method === 'POST') {
         const session = await getSession({ req });
 
-        const stripeCustomer = await stripe.customers.create({
-            email: session.user.email
-            // metadata
-        })
+        const user = await fauna.query<User>(q.Get(
+            q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(session.user.email)
+            )
+        ))
 
-        await fauna.query()
+        let customerId = user.data.stripe_customer_id
+
+        if (!customerId) {
+            const stripeCustomer = await stripe.customers.create({
+                email: session.user.email
+                // metadata
+            })
+            await fauna.query(
+                q.Update(
+                    q.Ref(q.Collection('users'), user.ref.id),
+                    {
+                        data: {
+                            stripe_customer_id: stripeCustomer.id,
+                        }
+                    }
+                )
+            )
+            customerId = stripeCustomer.id
+            //se tiver puxa a que ta no fauna se nao tiver cria uma nova
+        }
 
         const stripeCheckoutSession = await stripe.checkout.sessions.create({
-            customer: stripeCustomer.id,//id do stripe nao do fauna
+            customer: customerId,//id do stripe nao do fauna
             payment_method_types: ['card'],
             billing_address_collection: 'required',
             line_items: [{ price: 'price_1K7RrlD1uMx7q7dmJ59vcYG7', quantity: 1 }],
