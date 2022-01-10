ccc1b1a1713822cdc73808b5ff36f12812f0a96a
jeffersoncharlles <contato@jefferdeveloper.com>
2022-01-09 22:07:39 -0300

    chore: post page test getserversideprops2 commit

--- src/tests/pages/Post.spec.tsx
+++ src/tests/pages/Post.spec.tsx
@@ -48,4 +48,43 @@ describe('Post page', () => {
             })
         )
     });
+
+    //tem que verificar depois
+    // it('loads initial data', async () => {
+    //     prismicMocked.mockReturnValueOnce({
+    //         getByUID: jest.fn().mockResolvedValueOnce({
+    //             data: {
+    //                 title: [
+    //                     { type: 'heading', text: 'Itiprap' }
+    //                 ],
+    //                 content: [
+    //                     { type: 'paragraph', text: 'Post excerpt' }
+    //                 ],
+    //             },
+    //             last_publication_date: '04-01-2021'
+    //         })
+    //     } as any)
+    //     useSessionMocked.mockReturnValueOnce({
+    //         activeSubscription: 'fake-active'
+    //     } as any)
+
+    //     const resp = await getServerSideProps({
+    //         params: {
+    //             slug: 'my-new-post'
+    //         }
+    //     } as any);
+
+    //     expect(resp).toEqual(
+    //         expect.objectContaining({
+    //             props: {
+    //                 post: {
+    //                     slug: 'my-new-post',
+    //                     title: 'Itiprap',
+    //                     content: '<p>Post excerpt</p>',
+    //                     updated_at: '10 de abril de 2021'
+    //                 }
+    //             }
+    //         })
+    //     )
+    // })
 })
